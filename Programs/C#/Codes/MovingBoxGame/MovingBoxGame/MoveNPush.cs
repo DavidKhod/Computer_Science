a6c4a8243a2a7daad93f75e5503bfcd02b6bafbb
DavidKhod <Davidk207@gmail.com>
2023-05-02 09:50:17 +0300

    Fix some bugs in the MovingNPush game,still has bugs

--- Programs/C#/Codes/MovingBoxGame/MovingBoxGame/MoveNPush.cs
+++ Programs/C#/Codes/MovingBoxGame/MovingBoxGame/MoveNPush.cs
@@ -7,7 +7,7 @@ namespace MovingBoxGame
         private string[,] board = new string[5,5];
         private Position playerPosition;
         private Position BoxPosition;
-        private Position placePosition;
+        private Position returnSpot;
 
         private string playerUI = $"{Convert.ToChar(2)}";
         private string BoxUI = $"{Convert.ToChar(15)}";
@@ -26,20 +26,20 @@ namespace MovingBoxGame
 
         public void InitBoard()
         {
-            board[0,0] = this.playerUI;
+            this.board[0,0] = this.playerUI;
             Position playerPosition = new Position(0, 0);
             Position BoxPosition = new Position(rnd.Next(1, 4), rnd.Next(1, 4));
-            board[BoxPosition.Y, BoxPosition.X] = this.BoxUI;
+            this.board[BoxPosition.Y, BoxPosition.X] = this.BoxUI;
             Position placePosition = new Position(rnd.Next(1, 4), rnd.Next(1, 4));
             if(placePosition.X == BoxPosition.X && placePosition.Y == BoxPosition.Y)
             {
                 placePosition.X = placePosition.X - 1;
                 placePosition.Y = placePosition.Y - 1;
             }
-            board[placePosition.Y, placePosition.X] = this.placeTuPutUI;
+            this.board[placePosition.Y, placePosition.X] = this.placeTuPutUI;
             this.playerPosition = playerPosition;
             this.BoxPosition = BoxPosition;
-            this.placePosition = placePosition;
+            this.returnSpot = placePosition;
         }
 
 
@@ -49,42 +49,44 @@ namespace MovingBoxGame
             //IF W
             if (direction == "W" || direction == "w")
             {
-                if (this.placePosition.Y + 1 == this.BoxPosition.Y)
+                if (this.playerPosition.Y - 1 == this.BoxPosition.Y)
                 {
-                    if (this.BoxPosition.Y + 1 != -1)
+                    if (this.BoxPosition.Y - 1 != -1)
                     {
-                        this.board[BoxPosition.Y, BoxPosition.X] = "_";
-                        this.board[placePosition.Y, placePosition.X] = "_";
-                        this.BoxPosition.Y++;
-                        this.placePosition.Y++;
-                        this.board[BoxPosition.Y, BoxPosition.X] = this.BoxUI;
-                        this.board[placePosition.Y, placePosition.X] = this.playerUI;
+                        this.board[this.BoxPosition.Y, this.BoxPosition.X] = "_";
+                        this.board[this.playerPosition.Y, this.playerPosition.X] = "_";
+                        this.BoxPosition.Y--;
+                        this.playerPosition.Y--;
+                        this.board[this.BoxPosition.Y, this.BoxPosition.X] = this.BoxUI;
+                        this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
                     }
                 }
             }
-            else if (this.placePosition.Y + 1 != -1)
+            else if (this.playerPosition.Y - 1 != -1)
             {
-                this.placePosition.Y++;
+                this.board[this.playerPosition.Y, this.playerPosition.X] = "_";
+                this.playerPosition.Y--;
                 this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
             }
             //If S
             if (direction == "s" || direction == "S")
             {
-                if (this.placePosition.Y - 1 == this.BoxPosition.Y)
+                if (this.playerPosition.Y + 1 == this.BoxPosition.Y)
                 {
-                    if (this.BoxPosition.Y - 1 != 5)
+                    if (this.BoxPosition.Y + 1 != 5)
                     {
-                        this.board[BoxPosition.Y, BoxPosition.X] = "_";
-                        this.board[placePosition.Y, placePosition.X] = "_";
-                        this.BoxPosition.Y--;
-                        this.placePosition.Y--;
-                        this.board[BoxPosition.Y, BoxPosition.X] = this.BoxUI;
-                        this.board[placePosition.Y, placePosition.X] = this.playerUI;
+                        this.board[this.BoxPosition.Y, this.BoxPosition.X] = "_";
+                        this.board[this.playerPosition.Y, this.playerPosition.X] = "_";
+                        this.BoxPosition.Y++;
+                        this.playerPosition.Y++;
+                        this.board[this.BoxPosition.Y, this.BoxPosition.X] = this.BoxUI;
+                        this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
                     }
                 }
-                else if (this.playerPosition.Y - 1 != 5)
+                else if (this.playerPosition.Y + 1 != 5)
                 {
-                    this.playerPosition.Y--;
+                    this.board[this.playerPosition.Y, this.playerPosition.X] = "_";
+                    this.playerPosition.Y++;
                     this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
                 }
             }
@@ -95,18 +97,19 @@ namespace MovingBoxGame
                 {
                     if (this.BoxPosition.X + 1 != 5)
                     {
-                        this.board[BoxPosition.Y, BoxPosition.X] = "_";
-                        this.board[placePosition.Y, placePosition.X] = "_";
+                        this.board[this.BoxPosition.Y, this.BoxPosition.X] = "_";
+                        this.board[this.playerPosition.Y, this.playerPosition.X] = "_";
                         this.BoxPosition.X++;
                         this.playerPosition.X++;
-                        this.board[BoxPosition.Y, BoxPosition.X] = this.BoxUI;
-                        this.board[placePosition.Y, placePosition.X] = this.playerUI;
+                        this.board[this.BoxPosition.Y, this.BoxPosition.X] = this.BoxUI;
+                        this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
                     }
                 }
-                else if (this.placePosition.X + 1 != 5)
+                else if (this.playerPosition.X + 1 != 5)
                 {
-                    this.placePosition.X++;
-                    this.board[this.placePosition.Y, this.placePosition.X] = this.playerUI;
+                    this.board[this.playerPosition.Y, this.playerPosition.X] = "_";
+                    this.playerPosition.X++;
+                    this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
                 }
             }
             //If A
@@ -116,27 +119,28 @@ namespace MovingBoxGame
                 {
                     if (this.BoxPosition.X - 1 != -1)
                     {
-                        this.board[BoxPosition.Y, BoxPosition.X] = "_";
-                        this.board[placePosition.Y, placePosition.X] = "_";
+                        this.board[this.BoxPosition.Y, this.BoxPosition.X] = "_";
+                        this.board[this.playerPosition.Y, this.playerPosition.X] = "_";
                         this.BoxPosition.X--;
                         this.playerPosition.X--;
-                        this.board[BoxPosition.Y, BoxPosition.X] = this.BoxUI;
-                        this.board[placePosition.Y, placePosition.X] = this.playerUI;
+                        this.board[this.BoxPosition.Y, this.BoxPosition.X] = this.BoxUI;
+                        this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
                     }
                 }
             }
-            else if (this.placePosition.X - 1 != -1)
+            else if (this.playerPosition.X - 1 != -1)
             {
-                this.placePosition.X--;
+                this.board[this.playerPosition.Y, this.playerPosition.X] = "_";
+                this.playerPosition.X--;
                 this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
             }
         }
 
         public override string ToString()
         {
-            this.board[this.placePosition.Y, this.placePosition.X] = placeTuPutUI;
-            this.board[this.BoxPosition.Y, this.BoxPosition.X] = BoxUI;
-            this.board[this.placePosition.Y, this.placePosition.X] = playerUI;
+            this.board[this.returnSpot.Y, this.returnSpot.X] = this.placeTuPutUI;
+            this.board[this.BoxPosition.Y, this.BoxPosition.X] = this.BoxUI;
+            this.board[this.playerPosition.Y, this.playerPosition.X] = this.playerUI;
             string print = $"Player: {playerUI},Box: {this.BoxUI},Place to put: {this.placeTuPutUI}\n";
             for (int i = 0; i < board.GetLength(0); i++)
             {

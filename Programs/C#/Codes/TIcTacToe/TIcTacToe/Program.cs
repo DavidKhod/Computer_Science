3fe92ec43086667432a29fa467cc5a03c63814dc
DavidKhod <Davidk207@gmail.com>
2023-05-02 08:02:07 +0300

    merging commits again

--- Programs/C#/Codes/TIcTacToe/TIcTacToe/Program.cs
+++ Programs/C#/Codes/TIcTacToe/TIcTacToe/Program.cs
@@ -7,13 +7,15 @@ namespace TIcTacToe
         static void Main(string[] args)
         {
             char desicion = ' ';
+            int cntX = 0,cntO = 0;
             do
             {
+                Console.Clear();
                 Tictactoe game = new Tictactoe();
                 game.PrintGame();
                 int step = 1;
                 int position;
-                while (game.IsWon() == false && step <= 9)
+                while (game.IsWon() == false)
                 {
                     if (step % 2 != 0)
                     {
@@ -53,11 +55,17 @@ namespace TIcTacToe
                 {
                     Console.WriteLine($"GAME OVER");
                     Console.WriteLine($"Player {game.WhoWon()} Won");
+                    if (game.WhoWon() == 'O')
+                        cntO++;
+                    else
+                        cntX++;
                 }
                 Console.WriteLine();
                 Console.Write("Keep going(y/n): ");
                 desicion = char.Parse(Console.ReadLine());
             } while (desicion == 'y' || desicion == 'Y');
+            Console.WriteLine($"X won {cntX} times");
+            Console.WriteLine($"O won {cntX} times");
         }
     }
 }

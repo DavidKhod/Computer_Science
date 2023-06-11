44c6e1ec58d3cd4a0fc33e72cbd9f1e4e2040585
David_KH <Davidkh207@gmail.com>
2023-06-11 21:10:07 +0300

    Commit temp

---
+++ Programs/C#/Codes/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void BubbleSort(int[] arr)
+        {
+            int min;
+            int tempnum;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                min = arr[i];
+                tempnum = 0;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j] < min)
+                    {
+                        min = arr[j];
+                        tempnum = arr[i];
+                        arr[i] = min;
+                        arr[j] = tempnum;
+                    }
+                }
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"|{arr[i]}|");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 6, 1, 12, 7, -3, 0, 5 };
+            PrintArray(arr);
+            BubbleSort(arr);
+            PrintArray(arr);
+        }
+    }
+}

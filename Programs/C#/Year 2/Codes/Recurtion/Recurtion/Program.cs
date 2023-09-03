dfd88eba467e63c61c077ff3729fcb24bc45723c
DavidKhod <Davidk207@gmail.com>
2023-09-03 12:50:25 +0300

    Converted the codes into year one and year two, and added a recurtion example

---
+++ Programs/C#/Year 2/Codes/Recurtion/Recurtion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recurtion
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return factorial(n - 1) * n;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number for a factorial: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(factorial(num));
+            
+        }
+    }
+}

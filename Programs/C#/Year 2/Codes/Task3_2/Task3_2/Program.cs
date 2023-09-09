4462a2f8d426d4a635b8a9aa8790da2c62243af7
David_KH <Davidkh207@gmail.com>
2023-09-09 20:17:03 +0300

    Added HW task 3_2 and task WP1

---
+++ Programs/C#/Year 2/Codes/Task3_2/Task3_2/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Task3_2
+{
+    class Program
+    {
+        // This function finds the biggest even digit in a given integer 'num'.
+        // If no even digit is found, it returns -1.
+        static int BiggestEvenNum(int num)
+        {
+            if (num / 10 == 0)
+            {
+                if (num % 2 == 0)
+                    return num;
+                return -1;
+            }
+            else
+            {
+                int temp = BiggestEvenNum(num / 10);
+                if (temp == -1 && (num % 10) % 2 == 0)
+                {
+                    return (num % 10);
+                }
+                else
+                {
+                    if (temp != -1 && (num % 10) % 2 == 0)
+                        return Math.Max((num % 10), temp);
+                    else
+                        return temp;
+                }
+            }
+        }
+
+        // This function checks if all the digits in a given integer 'num' are either all even or all odd.
+        static bool FullOfEvenOrOdd(int num)
+        {
+            num = Math.Abs(num);
+            if (num < 10)
+            {
+                return true;
+            }
+            else
+            {
+                if ((((num / 10) % 10) % 2 == (num % 10) % 2))
+                    return FullOfEvenOrOdd(num / 10);
+                return false;
+            }
+        }
+
+        // This function performs integer division (x / y) using recursion.
+        static int Divide(int x, int y)
+        {
+            if (y == 1)
+            {
+                return x;
+            }
+            if (x < y)
+            {
+                return 0;
+            }
+            return Divide(x - y, y) + 1;
+        }
+
+        // This function calculates the remainder of integer division (x % y) using recursion.
+        static int Mod(int x, int y)
+        {
+            if (x < y)
+            {
+                return x;
+            }
+            return Mod(x - y, y);
+        }
+
+        // This function checks if x is a multiple of y using recursion.
+        static bool IsMultipliable(int x, int y)
+        {
+            if (x < y)
+            {
+                if (x == 0)
+                    return true;
+                return false;
+            }
+            else
+            {
+                return IsMultipliable(x - y, y);
+            }
+        }
+
+        // This function calculates the sum of odd digits from 1 to 'num' using recursion.
+        static int SumOfOddDigitsUntilNum(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                if (num % 2 == 0)
+                    return SumOfOddDigitsUntilNum(num - 1);
+                return SumOfOddDigitsUntilNum(num - 1) * num;
+            }
+        }
+
+        // This function performs multiplication of two integers 'x' and 'y' using recursion.
+        static int Times(int x, int y)
+        {
+            if (x == 1)
+            {
+                return y;
+            }
+            else
+            {
+                return Times(x - 1, y) + y;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The smallest even number is: {BiggestEvenNum(num)}");
+
+            bool allEvenOrOdd = FullOfEvenOrOdd(num);
+            if (allEvenOrOdd)
+                Console.WriteLine($"The number is full of Odd or Even numbers");
+            else
+                Console.WriteLine($"The number is not full of Odd or Even numbers");
+
+            Console.Write("X: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{x}/{y} == {Divide(x, y)}");
+            Console.WriteLine($"{x}%{y} == {Mod(x, y)}");
+
+            bool multi = IsMultipliable(x, y);
+            if (multi)
+                Console.WriteLine($"{x}%{y} == 0");
+            else
+                Console.WriteLine($"{x}%{y} != 0");
+
+            Console.WriteLine($"The sum of odd numbers from 1 to {num} is: {SumOfOddDigitsUntilNum(num)}");
+
+            Console.WriteLine($"{x}*{y} == {Times(x, y)}");
+        }
+    }
+}
---
+++ Programs/C#/Year 2/Codes/WP1_RecursionNumbers/WP1_RecursionNumbers/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace WP1_RecursionNumbers
+{
+    class Program
+    {
+        // 1.
+        // כתןב פעולה שמקבלת מספר שלם חיובי ומחזירה את מספר ספרותיו
+        static int CountLength(int num)
+        {
+            if (num / 10 == 0)//if its a one digit number
+                return 1;
+            //if the number has two digits, meaning x>=10 
+            return CountLength(num / 10) + 1;
+        }
+
+
+        // 2.
+        // כתוב פעולה שמקבלת מספר שלם חיובי ומחזירה את סכום הספרות במספר
+        static int SumOfDigits(int num)
+        {
+            if (num / 10 == 0)
+                return num;
+            else
+                return SumOfDigits(num / 10) + num % 10;
+        }
+
+
+        // 3.
+        // כתוב פעולה המקבלת מספר שלם חיובי ומחזירה את מספר הספרות הזוגיות במספר
+        static int CountEvenDigits(int num)
+        {
+            if (num / 10 == 0)
+            {
+                if (num % 2 == 0)//if the number is even return 1 else return 0
+                    return 1;
+                else
+                    return 0;
+            }
+            else
+            {
+                int temp = 0;
+                if (num % 2 == 0)//Check if the current number is even or odd
+                    temp = 1;
+                return CountEvenDigits(num / 10) + temp;
+            }
+        }
+
+
+        // 4. 
+        // כתוב פעולה המקבלת מספר שלם ומחזירה אמת אם הספרה 5 מופיעה במספר
+        static bool FindFive(int num)
+        {
+            if (num / 10 == 0)
+            {
+                if (num == 5)
+                    return true;
+                return false;
+            }
+            else
+            {
+                if (num % 10 == 5)
+                {
+                    return true;
+                }
+                return FindFive(num / 10);
+            }
+        }
+
+
+        // 5.
+        // כתוב פעולה המקבלת מספר שלם חיובי ומחזירה אמת אם קיימת במספר ספרה
+        // המתחלקת ב-3 ללא שארית
+        static bool DigitDivisableBy3(int num)
+        {
+            if (num / 10 == 0)
+            {
+                if (num % 3 == 0)
+                    return true;
+                return false;
+            }
+            else
+            {
+                if ((num % 10) % 3 == 0)
+                    return true;
+                return DigitDivisableBy3(num / 10);
+            }
+        }
+
+
+        // 6.
+        // כתוב פעולה המקבלת מספר שלם ומחזירה אמת אם כל ספרותיו זוגיות
+        static bool IsAllDigitEven(int num)
+        {
+            if (num / 10 == 0)
+            {
+                if (num % 2 == 0)
+                    return true;
+                return false;
+            }
+            else
+            {
+                if ((num % 10) % 2 != 0)
+                    return false;
+                return IsAllDigitEven(num / 10);
+
+            }
+        }
+
+
+        // 7.
+        // כתוב פעולה המקבלת מספר שלם חיובי 
+        // ומחזירה אמת אם כל ספרותיו מסודרות בסדר עולה משמאל לימין
+        static bool IsInRaisingOrderFromLeftToRight(int num)
+        {
+            if (num / 10 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                if (num % 10 < (num / 10) % 10)
+                    return false;
+                return IsInRaisingOrderFromLeftToRight(num / 10);
+            }
+        }
+
+
+        // 8.
+        // כתוב פעולה המקבלת מספר שלם חיובי ומחזירה את הספרה הקטנה ביותר במספר
+        static int SmallestNum(int num)
+        {
+            if (num < 100 && num > 10)
+            {
+                return Math.Min(num % 10, num / 10);
+            }
+            else
+            {
+                return Math.Min(SmallestNum(num / 10), num % 10);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The length of the number is: {CountLength(num)}");
+            Console.WriteLine($"The amount of even numbers is: {CountEvenDigits(num)}");
+            Console.WriteLine($"The sum of all digits is: {SumOfDigits(num)}");
+
+            bool fiveIsFound = FindFive(num);
+            if (fiveIsFound)
+                Console.WriteLine($"The number has the digit 5 within it");
+            else
+                Console.WriteLine($"The number doesn't have the digit 5 within it");
+
+            bool divisableBy3 = DigitDivisableBy3(num);
+            if (divisableBy3)
+                Console.WriteLine($"The number has a digit that is divisable by 3 within it");
+            else
+                Console.WriteLine($"The number doesn't have a digit that is divisable by 3 within it");
+
+            bool allDigitsEven = IsAllDigitEven(num);
+            if (allDigitsEven)
+                Console.WriteLine("All of the number's digits are even");
+            else
+                Console.WriteLine("Not all of the number's digits are even");
+
+            bool isRaisingOrder = IsInRaisingOrderFromLeftToRight(num);
+            if (isRaisingOrder)
+            {
+                Console.WriteLine("Raising order from left to right");
+            }
+            else
+            {
+                Console.WriteLine("Not raising order from left to right");
+            }
+
+            Console.WriteLine($"The smallest digit is: {SmallestNum(num)}");
+        }
+    }
+}

7310ec50dc95bd789bf80bc022a7a0840cd2ac56
DavidKhod <Davidk207@gmail.com>
2023-09-20 13:32:19 +0300

    Added task RecrtionWithArray (c#)

---
+++ Programs/C#/Year 2/Codes/RecurtionWithArray/RecurtionWithArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RecurtionWithArray
+{
+    class Program
+    {
+        static bool AscendingOrder(int[] arr, int i)
+        {
+            if (i == arr.Length-1)
+                return true;
+            else
+                if (arr[i] >= arr[i+1])
+                    return false;
+                return AscendingOrder(arr, i + 1);
+        }
+
+        static int sumOddValues(int[] arr)
+        {
+            return sumOddValues(arr, 0);
+        }
+
+        static int sumOddValues(int[] arr,int i)
+        {
+            if(i == arr.Length-1)
+            {
+                if (arr[i] % 2 != 0)
+                    return arr[i];
+                return 0;
+            }
+            else
+            {
+                if (arr[i] % 2 != 0)
+                    return sumOddValues(arr, i + 1) + arr[i];
+                return sumOddValues(arr, i + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

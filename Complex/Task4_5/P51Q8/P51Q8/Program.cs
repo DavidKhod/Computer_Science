6d0046112fa4ae5403f6d23fb2af0ccdf12b3512
David_KH <Davidkh207@gmail.com>
2023-11-24 20:58:33 +0200

    Transfered to Desktop and did some changes

---
+++ Complex/Task4_5/P51Q8/P51Q8/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace P51Q8
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static int[] CreateRandomArray(int size, int from, int to)
+        {
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(from, to + 1);
+            }
+            return arr;
+        }
+
+        static int[] CountAllScores(int[] arr)
+        {
+            int[] allScores = new int[101];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                allScores[arr[i]]++;
+            }
+            return allScores;
+        }
+
+        /// <summary>
+        /// Prints the array inputed
+        /// </summary>
+        /// <param name="arr">The array inputed</param>
+        static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"|{arr[i]}| ");
+            }
+            Console.WriteLine("");
+        }
+
+        static void PrintCountOfEveryScore(int[] arr)
+        {
+            int cnt = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                cnt = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] == i)
+                        cnt++;
+                }
+                Console.Write($"|{i}: {cnt} ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Option 1
+            int[] scores = CreateRandomArray(40, 0, 100);
+            int[] scoresCounter = CountAllScores(scores);
+            PrintArray(scores);
+            PrintArray(scoresCounter);
+            //Option 2
+            Console.WriteLine();
+            PrintCountOfEveryScore(scores);
+
+
+
+        }
+    }
+}
---
+++ Complex/Task4_5/P52Q9/P52Q9/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace P52Q9
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        /// <summary>
+        /// Creates an int Array from the field inputed by the user
+        /// </summary>
+        /// <param name="length">The length of the Array</param>
+        /// <param name="from">The smallest value of the field</param>
+        /// <param name="until">The largest value of field</param>
+        /// <returns>An int Array</returns>
+        static int[] CreateRndArray(int length, int from, int until)
+        {
+            int[] arr = new int[length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(from, until + 1);
+            }
+            return arr;
+        }
+
+        static void BubbleSort(int[] arr)
+        {
+            int tempnum;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        tempnum = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tempnum;
+                    }
+                }
+            }
+        }
+
+        static void PrintAllMatchingNums(int[] arr1, int[] arr2)
+        {
+            int i = 0, j = 0;
+            while (i < arr1.Length && j < arr2.Length)
+            {
+                if (arr1[i] == arr2[j])
+                {
+                    i++;
+                    j++;
+                    Console.WriteLine(arr1[i]);
+                }
+                if (arr1[i] > arr2[j])
+                    j++;
+                if (arr1[i] < arr2[j])
+                    i++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr1 = CreateRndArray(10, 1, 20);
+            BubbleSort(arr1);
+            int[] arr2 = CreateRndArray(10, 1, 20);
+            BubbleSort(arr2);
+        }
+    }
+}

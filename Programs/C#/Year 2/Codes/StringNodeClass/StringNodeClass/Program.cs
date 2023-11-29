9bf4edf4d5021ef736473092b529511e6a23cb53
DavidKhod <Davidk207@gmail.com>
2023-11-30 00:18:43 +0200

    Added a couple of funcions for IntNode and created a StringNode

---
+++ Programs/C#/Year 2/Codes/StringNodeClass/StringNodeClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringNodeClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new StringNode = ("Noam", new StringNode("Noam", new StringNode("David", new StringNode("Ori"))));
+        }
+    }
+}

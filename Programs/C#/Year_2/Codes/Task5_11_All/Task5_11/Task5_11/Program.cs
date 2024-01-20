6a26134b636fdc6e166efdb1d71f54e880b3cd62
David_KH <Davidkh207@gmail.com>
2024-01-21 00:54:01 +0200

    Did Task 5_11 of writing a function that inputs an integer and a linked list and returns a linked list of all the numbers from 0 to the integer that don't appear in the linked list, and a following chart of three other functions

---
+++ Programs/C#/Year_2/Codes/Task5_11_All/Task5_11/Task5_11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib;
+namespace Task5_11
+{
+    class Program
+    {
+        static Node<int> NumsFrom1ToNumThatDontAppearInList(Node<int> list,int num)
+        {
+            int temp = 1;
+            Node<int> rtnList = new Node<int>(0);
+            Node<int> pos = rtnList.Next;
+            while (list != null && temp < num)
+            {
+                if (!Exist(list, temp))
+                {
+                    pos = new Node<int>(temp);
+                    pos = pos.Next;
+                }
+                temp++;
+                list = list.Next;
+            }
+            return rtnList.Next;
+        }
+        //If n is the size of the list, the complexity of the operation will be O(n^2) meaning that the basic step is n that happens n times, so that O(n) * n = O(n^2)
+        static bool Exist(Node<int> list, int num)
+        {
+            while (list != null)
+            {
+                if (num == list.Value)
+                    return true;
+                list = list.Next;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

40eef95a9d9133bfe7ccf0542dbd06f31b1b989c
David_KH <Davidkh207@gmail.com>
2024-01-31 00:09:19 +0200

    Did the functions with Queue in Task7.0

---
+++ Programs/C#/Year_2/Codes/QueSelfExercise/QueSelfExercise/Program.cs
@@ -0,0 +1,108 @@
+﻿
+using System;
+
+class Program
+{
+
+    // 1
+    //  פעולה שתקבל תור של מספרים שלמים ותחזיר את מספר האיברים בתור
+    static int Length(Queue<int> que)
+    {
+        int cnt = 0;
+        for (Queue<int> pos = que; !pos.IsEmpty(); pos.Remove(), cnt++) { }
+        return cnt;
+    }
+
+    // 2
+    //  פעולה שתקבל תור של מספרים שלמים ומספר שלם נוסף
+    // הפעולה תחזיר אמת אם המספר הנתון נמצא בתור
+    // אחרת, הפעולה תחזיר שקר
+    static bool Exist(Queue<int> que, int num)
+    {
+        for (Queue<int> pos = que; !pos.IsEmpty(); pos.Remove())
+            if (pos.Head() == num)
+                return true;
+        return false;
+    }
+
+    // 3
+    //  פעולה שתקבל תור של מספרים שלמים 
+    // הפעולה תחזיר את הערך המקסימלי בתור
+    // הנחה: התור לא ריק
+    static int MaxValue(Queue<int> que)
+    {
+        int max = que.Head();
+        que.Remove();
+        for (Queue<int> pos = que; !pos.IsEmpty(); pos.Remove())
+            max = Math.Max(max, pos.Head());
+        return max;
+    }
+
+    // 4
+    //  פעולה שתקבל תור של מספרים שלמים 
+    // הפעולה תחזיר אמת אם הערכים בתור ממוינים בסדר עולה
+    // אחרת הפעולה תחזיר שקר
+    static bool IsSorted(Queue<int> que)
+    {
+        int prev = que.Head();
+        que.Remove();
+        for (Queue<int> pos = que; !pos.IsEmpty(); pos.Remove())
+        {
+            if (prev > pos.Head())
+                return false;
+            else
+                prev = pos.Head();
+        }
+        return true;
+    }
+
+    // 5
+    // x  פעולה שתקבל תור של מספרים שלמים ומספר שלם  
+    // מהתור x הפעולה תוציא את כל האיברים שערכם 
+    static Queue<int> RemoveNum(Queue<int> que, int num)
+    {
+        Queue<int> newQue = new Queue<int>();
+        for (Queue<int> pos = que; !pos.IsEmpty(); pos.Remove())
+            if (que.Head() != num)
+                newQue.Insert(que.Head());
+        return newQue;
+    }
+
+    // 6
+    //  פעולה שתקבל תור של מספרים שלמים    
+    // הפעולה תחזיר תור חדש שהוא העתק של התור
+    static Queue<int> Duplicate(Queue<int> que)
+    {
+        Queue<int> copy = new Queue<int>();
+        for (Queue<int> pos = que; !pos.IsEmpty(); pos.Remove())
+            copy.Insert(pos.Head());
+        return copy;
+    }
+
+    static Queue<int> CreateQueue(int n)
+    {
+        Queue<int> que = new Queue<int>();
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"{i + 1}-Enter an integer:");
+            que.Insert(int.Parse(Console.ReadLine()));
+            Console.WriteLine(que);
+        }
+        return que;
+    }
+
+    static void Main(string[] args)
+    {
+        // כתוב תוכנית בדיקה שתיצור תור של מספרים שלמים
+        // תכניס לתור מספר ערכים ותבדוק את נכונות כל הפעולות שכתבת
+        Queue<int> que = CreateQueue(5);
+        Console.WriteLine(que.ToString());
+        Console.WriteLine($"Length: {Length(que)}");
+        Console.WriteLine($"Exist(3): {Exist(que, 3)}");
+        Console.WriteLine($"MaxValue: {MaxValue(que)}");
+        Console.WriteLine($"IsSorted: {IsSorted(que)}");
+        Console.WriteLine($"RemoveNum 5: {RemoveNum(que, 5)}");
+        Console.WriteLine($"Duplicate: {Duplicate(que)}");
+    }
+}
+
---
+++ Programs/C#/Year_2/Codes/QueSelfExercise/QueSelfExercise/Queue.cs
@@ -0,0 +1,66 @@
+﻿public class Queue<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public Queue()
+    {
+        head = null;
+        tail = null;
+    }
+    public T Head()
+    {
+        return head.Value;
+    }
+
+    public void Insert(T x)
+    {
+        if (IsEmpty())
+        {
+            head = new Node<T>(x);
+            tail = head;
+        }
+        else
+        {
+            tail.Next = new Node<T>(x);
+            tail = tail.Next;
+        }
+    }
+
+    //Want to talk about why its not working
+
+    //public void Insert2(T x)
+    //{
+    //    if (IsEmpty())
+    //    {
+    //        head = new Node<T>(x);
+    //        head.Next = tail;
+    //    }
+    //    else
+    //    {
+    //        tail = new Node<T>(x, tail);
+    //        tail = tail.Next;
+    //    }
+    //}
+
+    public T Remove()
+    {
+        T value = head.Value;
+        head = head.Next;
+        return value;
+    }
+
+    public bool IsEmpty()
+    {
+        return head == null;
+    }
+
+
+    public override string ToString()
+    {
+        if (IsEmpty())
+            return "";
+        return $"{head.ToString()}";
+    }
+}
+

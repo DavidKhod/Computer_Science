5e58ecc404c5e038e524e00a3f752a2bdd8195a2
David_KH <Davidkh207@gmail.com>
2024-01-31 00:03:35 +0200

    Did Task 7.0 and added Queue class to Mylib

---
+++ Programs/C#/C#Things/LIbs/LibProject/MyLib/MyLib/Queue.cs
@@ -0,0 +1,51 @@
+﻿public class Queue<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public Queue()
+    {
+        head = null;
+        tail = null;
+    }
+
+    public T Head()
+    {
+        return head.Value;
+    }
+
+    public void Insert(T x)
+    {
+        if (IsEmpty())
+        {
+            head = new Node<T>(x);
+            tail = head;
+        }
+        else
+        {
+            tail.Next = new Node<T>(x);
+            tail = tail.Next;
+        }
+    }
+
+    public T Remove()
+    {
+        T value = head.Value;
+        head = head.Next;
+        return value;
+    }
+
+    public bool IsEmpty()
+    {
+        return head == null;
+    }
+
+
+    public override string ToString()
+    {
+        if (IsEmpty())
+            return "";
+        return $"{head.ToString()}";
+    }
+}
+

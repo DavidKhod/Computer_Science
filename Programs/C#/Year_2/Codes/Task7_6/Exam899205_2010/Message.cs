30ba3685d3d9cd255f68823ec0310cf984e470d9
DavidKhod <Davidk207@gmail.com>
2024-03-13 18:57:15 +0200

    Started doing task 7_6 which a folder of bagrut exams

---
+++ Programs/C#/Year_2/Codes/Task7_6/Exam899205_2010/Message.cs
@@ -0,0 +1,23 @@
+﻿
+public class Message
+{
+    public string sender { get; }
+    public string content { get; }
+    private static int ID;
+    public int id { get; }
+    public int bSize { get; }
+
+    public Message(string sender, string content, int bSize)
+    {
+        this.sender = sender;
+        this.content = content;
+        this.bSize = bSize;
+        id = ID++;
+    }
+
+    public override string ToString()
+    {
+        return $"Sender: {sender}\nId: {id}\nBit size: {bSize}\nContent: {content}";
+    }
+}
+
---
+++ Programs/C#/Year_2/Codes/Task7_6/Exam899205_2010/MessageBox.cs
@@ -0,0 +1,82 @@
+﻿public class MessageBox
+{
+    public string owner { get; }
+    private Queue<Message> messages;
+    private Queue<Message> bin;
+    public int max_capacity { get; set; }
+    private int activeCap;
+    private int binCap;
+    private int currentCap;
+
+
+    public MessageBox(string owner)
+    {
+        this.owner = owner;
+        messages = new Queue<Message>();
+        bin = new Queue<Message>();
+        activeCap = 0;
+        max_capacity = 100;
+        currentCap = binCap + activeCap;
+    }
+
+    public bool AddMessage(Message m)
+    {
+        if (currentCap + m.bSize > max_capacity)
+        {
+            while (!bin.IsEmpty() && currentCap + m.bSize > max_capacity)
+            {
+                if (!bin.IsEmpty())
+                {
+                    binCap -= bin.Head().bSize;
+                    UpdateCurrentSize();
+                    bin.Remove();
+                }
+            }
+            if (currentCap + m.bSize <= max_capacity)
+            {
+                messages.Insert(m);
+                activeCap += m.bSize;
+                UpdateCurrentSize();
+                return true;
+            }
+            else
+            {
+                UpdateCurrentSize();
+                return false;
+            }
+        }
+        messages.Insert(m);
+        activeCap += m.bSize;
+        UpdateCurrentSize();
+        return true;
+    }
+
+    public int RemoveFromBin()
+    {
+        if (!bin.IsEmpty())
+            return bin.Remove().id;
+        return -1;
+    }
+
+    public void EmptyBin()
+    {
+        bin = new Queue<Message>();
+    }
+
+    public int GetActiveSize()
+    {
+        return activeCap;
+    }
+
+    public int GetBinSize()
+    {
+        return binCap;
+    }
+
+    private void UpdateCurrentSize()
+    {
+        currentCap = binCap + activeCap;
+    }
+
+}
+

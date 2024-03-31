b86f6cc429be87b39aab10a8bee0df3bb55e1b1c
DavidKhod <Davidk207@gmail.com>
2024-03-31 18:13:43 +0300

    Added Task1_1

---
+++ Programs/C#/Year_2/Codes/Task7_6/27_3_2024/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace _27_3_2024
+{
+    internal class Program
+    {
+        //Q1
+        //Entry: The function inputs a generic queue.
+        //Exit: The function returns a copy of the queue.
+        static Queue<T> Duplicate<T>(Queue<T> current)
+        {
+            Queue<T> copy = new Queue<T>();
+            Queue<T> temp = new Queue<T>();
+            while (!current.IsEmpty())
+            {
+                temp.Insert(current.Remove());
+            }
+            while (!temp.IsEmpty())
+            {
+                current.Insert(temp.Head());
+                copy.Insert(temp.Remove());
+            }
+            return copy;
+        }
+
+        //Entry: The function inputs a queue and an integer.
+        /*Exit: The function returns True if there are two numbers in the queue
+                that their sum is equal to the integer inputed, False if not.
+        */
+        static bool TwoSum(Queue<int> que, int x)
+        {
+            int temp;
+            while (!que.IsEmpty())
+            {
+                temp = que.Remove();
+                Queue<int> tmpQ = Duplicate<int>(que);
+                while (tmpQ.IsEmpty())
+                {
+                    if (tmpQ.Remove() + temp == x)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        //Q2
+        public class TwoQueue
+        {
+            private Queue<int> numbers;
+            private Queue<int> sums;
+
+            public TwoQueue()
+            {
+                numbers = new Queue<int>();
+                sums = new Queue<int>();
+            }
+
+            public void Insert(int x)
+            {
+                if (sums.IsEmpty() && numbers.IsEmpty())
+                {
+                    sums.Insert(x);
+                    numbers.Insert(x);
+                }
+                else
+                {
+                    Queue<int> copyOfSums = Duplicate<int>(sums);
+                    int lastNumOfSums = copyOfSums.Remove();
+                    while (!copyOfSums.IsEmpty())
+                        lastNumOfSums = copyOfSums.Remove();
+                    sums.Insert(lastNumOfSums + x);
+                    numbers.Insert(x);
+                }
+            }
+
+            private Queue<T> Duplicate<T>(Queue<T> current)
+            {
+                Queue<T> copy = new Queue<T>();
+                Queue<T> temp = new Queue<T>();
+                while (!current.IsEmpty())
+                {
+                    temp.Insert(current.Remove());
+                }
+                while (!temp.IsEmpty())
+                {
+                    current.Insert(temp.Head());
+                    copy.Insert(temp.Remove());
+                }
+                return copy;
+            }
+
+            public Queue<int> GetNums(int x)
+            {
+                Queue<int> temp = Duplicate<int>(numbers);
+                Queue<int> newQueue = new Queue<int>();
+                int sum = 0;
+                while (sum != x)
+                {
+                    sum += temp.Head();
+                    newQueue.Insert(temp.Remove());
+                }
+                return newQueue;
+            }
+
+            public void EraseNum(int x)
+            {
+                Queue<int> newSums = new Queue<int>();
+                Queue<int> newNums = new Queue<int>();
+                int sum = 0;
+                while (!numbers.IsEmpty())
+                {
+                    if (numbers.Head() == x)
+                        numbers.Remove();
+                    if (!numbers.IsEmpty())
+                    {
+                        sum += numbers.Head();
+                        newNums.Insert(numbers.Head());
+                        newSums.Insert(sum);
+                    }
+                }
+                this.numbers = newNums;
+                this.sums = newSums;
+            }
+        }
+        public class TwoStack
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Programs/C#/Year_2/Codes/task1_1/Program.cs
@@ -0,0 +1,98 @@
+﻿static void PrintLongest(string s1, string s2)
+{
+    if (s1.Length > s2.Length)
+        Console.WriteLine(s1);
+    else if (s2.Length > s1.Length)
+        Console.WriteLine(s2);
+    else Console.WriteLine($"{s1} {s2}");
+}
+
+static string ReturnLongest(string s1, string s2)
+{
+    if (s1.Length > s2.Length)
+        return s1;
+    else if (s2.Length > s1.Length)
+        return s2;
+    else
+        return s1 += s2;
+}
+
+static void PrintReverse(string s)
+{
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        Console.Write(s[i]);
+    }
+}
+
+static string ReturnReverse(string s)
+{
+    string rev = "";
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        rev += s[i];
+    }
+    return rev;
+}
+
+static void PrintZigZag(string s)
+{
+    for (int i = 0; i < s.Length / 2; i++)
+    {
+        Console.Write(s[i]);
+        Console.Write(s[s.Length - i]);
+    }
+}
+
+static char BiggestChar(string s)
+{
+    char max = s[0];
+    for (int i = 1; i < s.Length; i++)
+    {
+        if (max < s[i])
+            max = s[i];
+    }
+    return max;
+}
+
+static int MinPlace(string s)
+{
+    char min = 'z';
+    int place = -1;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] < min)
+        {
+            min = s[i];
+            place = i;
+        }
+    }
+    return place;
+}
+
+static int CountChar(string s, char let)
+{
+    int count = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] == let)
+            count++;
+    }
+    return count;
+}
+
+static bool isSorted(string s)
+{
+
+    for (int i = 1; i < s.Length; i++)
+    {
+        if (s[i] < s[i - 1])
+            return false;
+    }
+    return true;
+}
+
+
+
+
+

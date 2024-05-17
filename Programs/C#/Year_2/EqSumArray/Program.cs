c490cac35cb6927fa3813ea4373431b1b2461edd
DavidKhod <Davidk207@gmail.com>
2024-05-17 11:02:30 +0300

    Saved after a long time

---
+++ Programs/C#/Year_2/EqSumArray/Program.cs
@@ -0,0 +1,22 @@
+﻿static bool EqSum(int[] arr)
+{
+    return EqSum(arr,0,arr.Length-1);
+}
+
+static bool EqSum(int[] arr, int i,int j)
+{
+    if(i+1 == j)
+        return true;
+    if(arr[i] + arr[j] != arr[i+1] + arr[j-1])
+        return false;
+    return EqSum(arr,i+1,j-1);
+}
+
+static bool EqSum(int[] arr, int i)
+{
+    if(i == arr.Length/2)
+        return true;
+    if(arr[i] + arr[arr.Length-1-i] != arr[i+1] + arr[arr.Length-1-i])
+        return false;
+    return EqSum(arr,i+1);
+} 
---
+++ Programs/C#/Year_2/ToNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿static int ToNumber(Queue<int> q)
+{
+    return ToNumber(q,10);
+}
+
+static int ToNumber(Queue<int> q, int times)
+{
+    if (q.IsEmpty()) { return 0; }
+    return q.Remove()*times + ToNumber(q, times * 10);
+}
